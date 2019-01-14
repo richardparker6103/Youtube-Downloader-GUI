309a97ad27f60671f8882ec2a0969f9300f60d08
richardparker6103 <richardparker6103@mailinator.com>
2019-01-14 18:45:31 -0200

    Update ytconsole.py

--- ytconsole.py
+++ ytconsole.py
@@ -1,75 +1,82 @@
 # coding: utf-8
 
-import os
-import argparse
-import sys
+import os, argparse, shutil
+import sys, youtube_dl
 
-pasta_local = os.path.expanduser('~/') + 'Desktop/'
-disk_path = 'E:/'
+desktop = os.path.expanduser('~/') + 'Desktop/'
+ex = 'https://www.youtube.com/watch?v=C0DPdy98e4c'
 
-def info(link):
+def get_info(link):
         try:
                 ydl = youtube_dl.YoutubeDL()
                 meta = ydl.extract_info(link, download=False)
                 #Global titulo, para nome final de arquivo
                 global titulo
                 titulo = meta['title']
-                #Converter segundos para minutos
-                m1 = meta['duration']
-                minutes = m1/60
                 print '\nTitulo: {}'.format(meta['title'])
-                print 'Duracao: {} segundos / {} minutos'.format(meta['duration'], int(minutes))
-                print 'Formato: {}'.format(meta['format'])
+                print 'Duracao: {} segundos'.format(meta['duration'])
                 print 'Data de upload: {}'.format(meta['upload_date'])
                 print 'Uploader: {}'.format(meta['uploader'])
                 print 'Visualizacoes: {}'.format(meta['view_count'])
         except TypeError:
                 exit("\n[*] Link invalido, impossivel obter informacoes")
 		
-def baixar_video(link):
-	yt = os.system('youtube-dl --format mp4 %s' % link)
-def baixar_audio(link):
-    	yt = os.system('youtube-dl -x --audio-format mp3 %s' % link)
-def enviar(destino):
-    if metodo == 'audio':
-        cp = os.system('move *.mp3 %s' %destino)
-        if cp == 1:
-            exit('\n[*]O arquivo nao pode ser enviado\n')
-        print 'Arquivo enviado para %s'
-    else:
-        cp = os.system('move *.mp4 %s' %destino)
-        if cp == 1:
-            exit('\n[*]O arquivo nao pode ser enviado\n')
-        print 'Arquivo enviado para %s' % destino
+#get_video_() and get_audio_() downloads content from the web and automatically saves on Desktop, see $desktop
+                
+def get_video_(link):
+        global file_format
+        yt = os.system('youtube-dl --format mp4 --output "{}{}.mp4" {}'.format(desktop, titulo, link))
+        file_format = '.mp4'
+        print "\n[*] {}{} concluido ".format(titulo,file_format)
+        
+def get_audio_(link):
+        global file_format
+        yt = os.system('youtube-dl -x --audio-format mp3 --output "{}{}.mp4" {}'.format(desktop, titulo, link))
+        file_format = '.mp3'
+        print "\n[*] {}{} concluido ".format(titulo,file_format)
+        
+def send_(device):
+        try:
+                src_file = desktop + titulo + file_format
+                shutil.copy(src_file, device)
+                print "[*] {} enviado para {}.".format(src_file, device)
+        except IOError:
+                print "[*] Nao foi possivel enviar para {}\nveja se o dispositivo esta conectado e tente novamente como administrador.\n".format(device)
+        
 		
 if __name__=="__main__":
-    if len(sys.argv) < 2:
-        exit("Uso: python ytconsole.py -l https://youtube.com/watch?video=7876asxXa87s")
-    parser = argparse.ArgumentParser(prog="os2.py", usage="os2.py -l [YOUTUBE_LINK]")
-    parser.add_argument("-l", "--link", help='selecione o link do video')
-    parser.add_argument('-v', '--video', help='baixar somente video', action='store_true')
-    parser.add_argument('-a', '--audio', help='baixar somente audio', action='store_true')
-    parser.add_argument('-p', '--pendrive', help='direto pro pendrive', action='store_true')
-    args = parser.parse_args()
-    pendrive = args.pendrive
-    link = args.link
-    video = args.video
-    audio = args.audio
-    
-    if video:
-        metodo = 'video'
-        baixar_video(link)
-        if pendrive:
-            enviar(disk_path)
-        else:
-            enviar(pasta_local)
-    if audio:
-        metodo = 'audio'
-        baixar_audio(link)
-        if pendrive:
-            enviar(disk_path)
-        else:
-            enviar(pasta_local)
+        if len(sys.argv) < 2:
+                exit("Uso: python ytconsole.py --help")
+        parser = argparse.ArgumentParser()
+        parser.add_argument("-l", "--link", help='Link do conteudo')
+        parser.add_argument('-v', '--video', help='Baixar somente video', action='store_true')
+        parser.add_argument('-a', '--audio', help='Baixar somente audio', action='store_true')
+        parser.add_argument('-i', '--info', help='Retornar informacoes sobre o link e sair', action='store_true')
+        parser.add_argument('-d', '--device', help='Baixar e enviar para dispositivo configurado.', action='store_true')
+        args = parser.parse_args()
+        link = args.link
+        video = args.video
+        audio = args.audio
+        info = args.info
+        device = args.device
+        if info:
+                get_info(link)
+        elif video:
+                get_info(link)
+                get_video_(link)
+                if device:
+                        send_(disk_path)
+        elif audio:
+                get_info(link)
+                get_audio_(link)
+                if device:
+                        send_(disk_path)
+                
+        
+        
+
+
+
 
 		
 	
