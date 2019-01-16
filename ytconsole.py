f209dfd17502e56f8664b69aff91187b42bdd56d
richardparker6103 <richardparker6103@mailinator.com>
2019-01-16 15:00:40 -0200

    Update ytconsole.py

--- ytconsole.py
+++ ytconsole.py
@@ -3,15 +3,16 @@
 import os, argparse, shutil
 import sys, youtube_dl
 
+script_dir = os.getcwd()
 desktop = os.path.expanduser('~/') + 'Desktop/'
 ex = 'https://www.youtube.com/watch?v=C0DPdy98e4c'
 
 def get_info(link):
         try:
+                global titulo
                 ydl = youtube_dl.YoutubeDL()
                 meta = ydl.extract_info(link, download=False)
                 #Global titulo, para nome final de arquivo
-                global titulo
                 titulo = meta['title']
                 print '\nTitulo: {}'.format(meta['title'])
                 print 'Duracao: {} segundos'.format(meta['duration'])
@@ -35,16 +36,17 @@ def get_audio_(link):
         file_format = '.mp3'
         print "\n[*] {}{} concluido ".format(titulo,file_format)
         
-def send_(device):
+def send_(disk_path):
         try:
                 src_file = desktop + titulo + file_format
-                shutil.copy(src_file, device)
-                print "[*] {} enviado para {}.".format(src_file, device)
+                shutil.copy(src_file, disk_path)
+                print "[*] {} enviado para {}.".format(src_file, disk_path)
         except IOError:
-                print "[*] Nao foi possivel enviar para {}\nveja se o dispositivo esta conectado e tente novamente como administrador.\n".format(device)
+                print "[*] Nao foi possivel enviar para {}\nveja se o dispositivo esta conectado e tente novamente como administrador.\n".format(disk_path)
         
 		
 if __name__=="__main__":
+        global disk_path
         if len(sys.argv) < 2:
                 exit("Uso: python ytconsole.py --help")
         parser = argparse.ArgumentParser()
@@ -52,31 +54,26 @@ if __name__=="__main__":
         parser.add_argument('-v', '--video', help='Baixar somente video', action='store_true')
         parser.add_argument('-a', '--audio', help='Baixar somente audio', action='store_true')
         parser.add_argument('-i', '--info', help='Retornar informacoes sobre o link e sair', action='store_true')
-        parser.add_argument('-d', '--device', help='Baixar e enviar para dispositivo configurado.', action='store_true')
+        parser.add_argument('-d', '--disk_path', help='Baixar e enviar para dispositivo configurado.')
         args = parser.parse_args()
         link = args.link
         video = args.video
         audio = args.audio
         info = args.info
-        device = args.device
+        disk_path= args.disk_path
+        if disk_path == None:
+                disk_path = False
         if info:
                 get_info(link)
         elif video:
                 get_info(link)
                 get_video_(link)
-                if device:
+                if disk_path:
                         send_(disk_path)
         elif audio:
                 get_info(link)
                 get_audio_(link)
-                if device:
+                if disk_path:
                         send_(disk_path)
-                
-        
-        
 
-
-
-
-		
-	
+              
