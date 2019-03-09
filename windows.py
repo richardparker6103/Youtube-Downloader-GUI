1b4feb29df94f2cdcc19eb1658379b70d6f55675
richardparker6103 <richardparker6103@mailinator.com>
2019-03-09 17:17:13 -0300

    str(dir) added

--- windows.py
+++ windows.py
@@ -4,5 +4,5 @@ _dir_ = os.getcwd()
 
 os.system('pip install pyinstaller')
 os.system('pyinstaller --noconsole --onefile -i ytbico.ico ytdown.py')
-os.system('copy dist/ytdown.py {}'.format(dir + '/'))
+os.system('copy dist/ytdown.py {}'.format(str(dir) + '/'))
 print '[*] Executavel criado.'
